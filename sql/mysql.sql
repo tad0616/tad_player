e36d4273aa37fa8ce88c924f5d6f07e2d8ffe7d0
Tad <tad0616@gmail.com>
2013-09-06 09:09:35 +0800

    add sql

---
+++ sql/mysql.sql
@@ -0,0 +1,42 @@
+CREATE TABLE `tad_player` (
+  `psn` smallint(5) unsigned NOT NULL auto_increment,
+  `pcsn` smallint(5) unsigned NOT NULL default '0',
+  `title` varchar(255) NOT NULL default '',
+  `creator` varchar(255) NOT NULL default '',
+  `location` varchar(255) NOT NULL default '',
+  `image` varchar(255) NOT NULL default '',
+  `info` varchar(255) NOT NULL default '',
+  `uid` smallint(5) unsigned NOT NULL default '0',
+  `post_date` varchar(255) NOT NULL default '',
+  `enable_group` varchar(255) NOT NULL default '',
+  `counter` smallint(5) unsigned NOT NULL default '0',
+  `width` smallint(5) unsigned NOT NULL default '0',
+  `height` smallint(5) unsigned NOT NULL default '0',
+  `sort` smallint(5) unsigned NOT NULL default '0',
+  `content` text NOT NULL,
+  `youtube` varchar(255) NOT NULL default '',
+  `logo` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`psn`)
+)ENGINE=MyISAM;
+
+
+CREATE TABLE `tad_player_cate` (
+  `pcsn` smallint(5) unsigned NOT NULL auto_increment,
+  `of_csn` smallint(5) unsigned NOT NULL default '0',
+  `title` varchar(255) NOT NULL default '',
+  `enable_group` varchar(255) NOT NULL default '',
+  `enable_upload_group` varchar(255) NOT NULL default '',
+  `sort` smallint(5) unsigned NOT NULL default '0',
+  `width` smallint(5) unsigned NOT NULL default '0',
+  `height` smallint(5) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`pcsn`)
+)ENGINE=MyISAM;
+
+CREATE TABLE `tad_player_rank` (
+  `col_name` varchar(255) NOT NULL,
+  `col_sn` smallint(5) unsigned NOT NULL,
+  `rank` tinyint(3) unsigned NOT NULL,
+  `uid` smallint(5) unsigned NOT NULL,
+  `rank_date` datetime NOT NULL,
+  PRIMARY KEY (`col_name`,`col_sn`,`uid`)
+) ENGINE = MYISAM ;

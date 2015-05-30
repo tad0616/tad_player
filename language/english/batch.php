1999744cd638aeda6f26357afa5f40123446ebdd
mambax7 <mambax7@gmail.com>
2015-05-30 08:31:16 -0400

    Language & Help updates

--- language/english/batch.php
+++ language/english/batch.php
@@ -1,6 +1,5 @@
 <?php
-define("_MD_TADPLAYER_BATCH_UPLOAD_TO","Please upload videos to:");
-define("_MD_TADPLAYER_BATCH_OF_CSN","Category");
-define("_MD_TADPLAYER_BATCH_NEW_PCSN","Creat a new sub-category from the category on left side:");
-define("_MD_BATCH_SAVE","Save");
-
+define('_MD_TADPLAYER_BATCH_UPLOAD_TO', 'Please upload videos to:');
+define('_MD_TADPLAYER_BATCH_OF_CSN', 'Category');
+define('_MD_TADPLAYER_BATCH_NEW_PCSN', 'Create a new sub-category from the category on left side:');
+define('_MD_BATCH_SAVE', 'Save');

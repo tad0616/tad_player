fb80022ba91226a0089183a8676ea42b7ae58fe8
mambax7 <mambax7@gmail.com>
2017-09-02 04:16:28 -0400

    Type casting

--- admin/save_cate_sort.php
+++ admin/save_cate_sort.php
@@ -2,8 +2,8 @@
 /*-----------引入檔案區--------------*/
 include "../../../include/cp_header.php";
 
-$pcsn = intval($_POST['pcsn']);
-$sort = intval($_POST['sort']);
+$pcsn = (int)$_POST['pcsn'];
+$sort = (int)$_POST['sort'];
 $sql  = "update " . $xoopsDB->prefix("tad_player_cate") . " set `sort`='{$sort}' where pcsn='{$pcsn}'";
 $xoopsDB->queryF($sql) or die("Save Sort Fail! (" . date("Y-m-d H:i:s") . ")");
 

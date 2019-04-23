8d35d62315d6f259c6508318613c1933a9c3ab80
mambax7 <mambax7@gmail.com>
2019-04-23 19:09:33 -0400

    Untrusted files inclusion

--- batch_upload.php
+++ batch_upload.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/header.php';
-require_once "language/{$xoopsConfig['language']}/batch.php";
+require_once __DIR__ . "/language/{$xoopsConfig['language']}/batch.php";
 
 require_once $GLOBALS['xoops']->path('/modules/system/include/functions.php');
 $op = system_CleanVars($_REQUEST, 'op', '', 'string');
